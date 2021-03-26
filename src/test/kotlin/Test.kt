0eb4135fae3bfb2c95e4128322211566c06fff38
Alejandro <alejandro.miralles@wallapop.com>
2021-03-26 12:20:29 +0100

    Initial test

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,10 +1,34 @@
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import java.lang.Exception
 
 class Test {
 
     @Test
     fun `test`(){
         println("this is a test")
+        val inputString = arrayOf("10", "20", "0", "0", "n", "f").joinToString("\n")
+        val inputStream = ByteArrayInputStream(inputString.toByteArray())
+
+        System.setIn(inputStream)
+
+        val outputStream = ByteArrayOutputStream()
+        System.setOut(PrintStream(outputStream))
+
+        try {
+
+            MarsRover.main(emptyArray())
+        } catch (e: Exception){
+
+        }
+
+        val output = outputStream.toString().split("\n")
+
+        assertEquals("Rover is at x:0 y:1 facing:n", output[output.size-3])
+
     }
 
 }
