9762b18ace5349cd6818b01c2891a2d1b38a63aa
Alejandro <alejandro.miralles@wallapop.com>
2021-03-26 12:23:20 +0100

    added backward test

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -8,8 +8,7 @@ import java.lang.Exception
 class Test {
 
     @Test
-    fun `test`(){
-        println("this is a test")
+    fun `it should move forward`(){
         val inputString = arrayOf("10", "20", "0", "0", "n", "f").joinToString("\n")
         val inputStream = ByteArrayInputStream(inputString.toByteArray())
 
@@ -28,7 +27,31 @@ class Test {
         val output = outputStream.toString().split("\n")
 
         assertEquals("Rover is at x:0 y:1 facing:n", output[output.size-3])
-
     }
+
+
+    @Test
+    fun `it should move backward`(){
+        val inputString = arrayOf("10", "20", "0", "1", "n", "b").joinToString("\n")
+        val inputStream = ByteArrayInputStream(inputString.toByteArray())
+
+        System.setIn(inputStream)
+
+        val outputStream = ByteArrayOutputStream()
+        System.setOut(PrintStream(outputStream))
+
+        try {
+
+            MarsRover.main(emptyArray())
+        } catch (e: Exception){
+
+        }
+
+        val output = outputStream.toString().split("\n")
+
+        assertEquals("Rover is at x:0 y:0 facing:n", output[output.size-3])
+    }
+
+
 
 }
